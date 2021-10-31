39724fa7c4b01414042b443f1db20f0e20404c5b
Sean Chien <seanchien@tencent.com>
2021-10-31 19:03:51 +0800

    Add exam2_1 and exam2_2

---
+++ chapter2/exam2_1.cpp
@@ -0,0 +1,11 @@
+// exam2.1
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    a = 65;
+    cout << a << endl;
+    return 0;
+}
---
+++ chapter2/exam2_2.cpp
@@ -0,0 +1,13 @@
+// exam2.2
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    a = 65;
+    cout << a << endl;
+    a = 100;
+    cout << a << endl;
+    return 0;
+}
